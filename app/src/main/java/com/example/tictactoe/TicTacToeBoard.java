2f7f06a2a628bc9514d3cf5b05f754dcc5f7fc72
Taye-Staats <tayestaats@outlook.com>
2025-10-01 00:00:06 -0700

    "Starting to create tictactoeboard with demensions and colors"

--- app/src/main/java/com/example/tictactoe/TicTacToeBoard.java
+++ app/src/main/java/com/example/tictactoe/TicTacToeBoard.java
@@ -1,4 +1,40 @@
 package com.example.tictactoe;
 
-public class TicTacToeBoard {
+import android.content.Context;
+import android.content.res.TypedArray;
+import android.util.AttributeSet;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+
+public class TicTacToeBoard extends View {
+
+    private final int boardColor;
+    private final int xColor;
+    private final int oColor;
+    private final int winningLineColor;
+
+    public TicTacToeBoard(Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+
+        TypedArray a = context.getTheme().obtainStyledAttributes(attrs,
+                R.styleable.TicTacToeBoard, 0, 0);
+        try{
+            boardColor = a.getInteger(R.styleable.TicTacToeBoard_boardColor, 0 );
+            xColor = a.getInteger(R.styleable.TicTacToeBoard_XColor, 0);
+            oColor = a.getInteger(R.styleable.TicTacToeBoard_OColor, 0);
+            winningLineColor = a.getInteger(R.styleable.TicTacToeBoard_winningLineColor, 0);
+        } finally{
+            a.recycle();
+        }
+    }
+
+    @Override
+    protected void onMeasure(int width, int height){
+        super.onMeasure(width, height);
+
+        int dimensions = Math.min(getMeasuredWidth(), getMeasuredHeight());
+
+        setMeasuredDimension(dimensions, dimensions );
+    }
 }
