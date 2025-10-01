71e3ccb542658e33aac6a71a02f6fec20427f9d3
Taye-Staats <tayestaats@outlook.com>
2025-09-30 23:36:51 -0700

    "Adding game board page and connecting all the buttons"

--- app/src/main/java/com/example/tictactoe/GameDisplay.java
+++ app/src/main/java/com/example/tictactoe/GameDisplay.java
@@ -3,7 +3,6 @@ package com.example.tictactoe;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,34 +10,28 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class PlayerSetup extends AppCompatActivity {
-
-    private EditText player1;
-    private EditText player2;
+public class GameDisplay extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.player_setup);
-
-        player1 = findViewById(R.id.editTextText);
-        player2 = findViewById(R.id.editTextText2);
+        setContentView(R.layout.game_display);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-    public void submitButtonClick(View view)
+
+    public void playAgainButtonClick(View view)
     {
-        String player1Name = player1.getText().toString();
-        String player2Name = player2.getText().toString();
-
-        Intent intent = new Intent(this, GameDisplay.class);
-        intent.putExtra( "PLAYER NAMES", new String[] {player1Name, player2Name});
-        startActivity(intent);
-
+        // do fancy stuff
     }
 
+    public void homeButtonClick(View view)
+    {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+    }
 }
