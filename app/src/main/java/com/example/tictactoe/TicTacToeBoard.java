8154138679471a6d78be034308c85b4ab8d570fd
Taye-Staats <tayestaats@outlook.com>
2025-10-01 18:20:44 -0700

    "Tic Tac Toe Board created, x's and o's starting to be produced"

--- app/src/main/java/com/example/tictactoe/TicTacToeBoard.java
+++ app/src/main/java/com/example/tictactoe/TicTacToeBoard.java
@@ -2,6 +2,8 @@ package com.example.tictactoe;
 
 import android.content.Context;
 import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.view.View;
 
@@ -13,6 +15,9 @@ public class TicTacToeBoard extends View {
     private final int xColor;
     private final int oColor;
     private final int winningLineColor;
+    private final Paint paint = new Paint();
+
+    private int cellSize = getWidth()/3;
 
     public TicTacToeBoard(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
@@ -33,8 +38,60 @@ public class TicTacToeBoard extends View {
     protected void onMeasure(int width, int height){
         super.onMeasure(width, height);
 
-        int dimensions = Math.min(getMeasuredWidth(), getMeasuredHeight());
+        int dimension = Math.min(getMeasuredWidth(), getMeasuredHeight());
+        cellSize = dimension /3;
 
-        setMeasuredDimension(dimensions, dimensions );
+        setMeasuredDimension(dimension, dimension);
     }
+
+    @Override
+    protected void onDraw(Canvas canvas)
+    {
+        paint.setStyle(Paint.Style.STROKE);
+        paint.setAntiAlias(true);
+
+        drawGameBoard(canvas);
+
+        drawX(canvas, 2, 2);
+        drawO(canvas, 1, 1);
+    }
+
+    private void drawGameBoard(Canvas canvas)
+    {
+        paint.setColor(boardColor);
+        paint.setStrokeWidth(16);
+        for(int c = 1; c <3; c++)
+        {
+            canvas.drawLine(cellSize * c, 0, cellSize * c, canvas.getWidth(), paint);
+        }
+        for(int r = 1; r <3; r++)
+        {
+            canvas.drawLine( 0,cellSize * r, canvas.getWidth(), cellSize * r, paint);
+        }
+    }
+
+    private void drawX(Canvas canvas, int row, int col)
+    {
+        paint.setColor(xColor);
+        canvas.drawLine((float)((col + 1) * cellSize - cellSize *0.2),
+                        (float)(row * cellSize + cellSize *0.2),
+                        (float)(col * cellSize + cellSize *0.2),
+                        (float)((row + 1) * cellSize - cellSize *0.2),
+                        paint);
+        canvas.drawLine((float)(col * cellSize+ cellSize *0.2),
+                        (float)(row * cellSize+ cellSize *0.2),
+                        (float)((col + 1) * cellSize- cellSize *0.2),
+                        (float)((row + 1) * cellSize- cellSize *0.2),
+                        paint);
+    }
+    private void drawO(Canvas canvas, int row, int col)
+    {
+        paint.setColor(oColor);
+        canvas.drawOval((float)(col * cellSize+ cellSize *0.2),
+                (float)(row * cellSize+ cellSize *0.2),
+                (float)((col * cellSize + cellSize) - cellSize *0.2),
+                (float)((row * cellSize + cellSize)- cellSize *0.2),
+                paint);
+    }
+
 }
